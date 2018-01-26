ec9c053ee7685ea9124e3d74bbe2d4e7eb816808
willsinn <30275717+willsinn@users.noreply.github.com>
2018-01-26 16:47:24 -0500

    RegEx and postVars

--- sheets.js
+++ sheets.js
@@ -9,153 +9,59 @@
 //7. set function on timer
 
 
-function curriculum(start) {
+function curricRequests(start) {
 
     var start = start || 0;
     var threads = GmailApp.getInboxThreads(start, 5);
-    var sheet = SpreadsheetApp.getActiveSpreadsheet();
-       SpreadsheetApp.setActiveSpreadsheet(sheet.getSheets()[7]);
+    var sheet = SpreadsheetApp.getActiveSheet();
+
+    // var sheet = SpreadsheetApp.getActiveSpreadsheet();
+    //    SpreadsheetApp.setActiveSpreadsheet(sheet.getSheets()[7]);
 
     for (var i = 0; i < threads.length; i++) {
 
-      var post,      
+      var post,
           message = threads[i].getMessages()[0],
           content = message.getPlainBody();
 
       if (content) {
 
-        post = content.match(/Name:\s*([A-Za-z0-9\s]+)(\r?\n)/);
-        var username = (post && post[1]) ? post[1].trim() : 'No username';
+        post = content.match(/Location:\s*([A-Za-z0-9\.\-\/\s]+)(\r?\n)/);
+        var location = (post && post[1]) ? post[1].trim() : 'No Location';
 
-        post = content.match(/Email:\s*([A-Za-z0-9@.]+)/);
-        var email = (post && post[1]) ? post[1].trim() : 'No email';
+        post = content.match(/Name:\s*([A-Za-z0-9\.\-\/&\s, '&']+)(\r?\n)/);
+        var name = (post && post[1]) ? post[1].trim() : 'No Name';
 
-        post = content.match(/Subject:\s*([A-Za-z0-9\s]+)(\r?\n)/);
-        var subject = (post && post[1]) ? post[1].trim() : 'No subject';
+        post = content.match(/Prep Type:\s*([A-Za-z0-9\.\-\/\s]+)(\r?\n)/);
+        var prep = (post && post[1]) ? post[1].trim() : 'No Prep Type';
 
-        post = content.match(/Comments:\s*([\s\S]+)/);
-        var comment = (post && post[1]) ? post[1] : 'No comment';
+        post = content.match(/Current Grade:\s*([A-Za-z0-9\.\-\/\s]+)(\r?\n)/);
+        var grade = (post && post[1]) ? post[1].trim() : 'No Grade';
 
-        sheet.appendRow([username, email, subject, comment]);
+        post = content.match(/Session Duration:\s*([A-Za-z0-9\.\-\/\s]+)(\r?\n)/);
+        var duration = (post && post[1]) ? post[1].trim() : 'No Duration';
+
+        post = content.match(/Portal ID:\s*([0-9\s]+)(\r?\n)/);
+        var identify = (post && post[1]) ? post[1].trim() : 'No Portal ID';
+
+        post = content.match(/Date & Time of Next Session:\s*([A-Za-z0-9\.\-\/\s]+)(\r?\n)/);
+        var date = (post && post[1]) ? post[1].trim() : 'No Session Details';
+
+        post = content.match(/Request Details:\s*([A-Za-z0-9\.\-\/\s]+)(\r?\n)/);
+        var request = (post && post[1]) ? post[1].trim() : 'No Request';
+
+        post = content.match(/Number of Sessions:\s*([A-Za-z0-9\s]+)(\r?\n)/);
+        var amount = (post && post[1]) ? post[1] : 'No Session Amount';
+
+        post = content.match(/Initials:\s*([A-Za-z\s]+)\n/);
+        var requestee = (post && post[1]) ? post[1].trim() : 'No Initials';
+
+
+
+
+        sheet.appendRow([location, name, prep, grade, duration, identify, date, request, amount, requestee]);
 
       }
-      threads[i].moveToSpam();
+      // threads[i].moveToSpam();
     }
   };
-
-
-
-
-
-
-
-
-function processInboxToSheet() {
-
-  var start = start || 0;
-  var threads = GmailApp.getInboxThreads(start, 5);
-  var sheet = SpreadsheetApp.getActiveSheet();
-
-
-  for (var i = 0; i < threads.length; i++) {
-
-      var tmp,
-        message = threads[i].getMessages()[0],
-        subject = message.getSubject(),
-        content = message.getPlainBody();
-
-    if (content) {
-
-      tmp = content.match(/Name:\s*([A-Za-z0-9\s]+)(\r?\n)/);
-      var username = (tmp && tmp[1]) ? tmp[1].trim() : 'No username';
-
-      tmp = content.match(/Email:\s*([A-Za-z0-9@.]+)/);
-      var email = (tmp && tmp[1]) ? tmp[1].trim() : 'No email';
-
-      tmp = content.match(/Subject:\s*([A-Za-z0-9\s]+)(\r?\n)/);
-      var subject = (tmp && tmp[1]) ? tmp[1].trim() : 'No subject';
-
-      tmp = content.match(/Comments:\s*([\s\S]+)/);
-      var comment = (tmp && tmp[1]) ? tmp[1] : 'No comment';
-
-      sheet.appendRow([username, email, subject, comment]);
-
-    }
-    GmailApp.moveThreadToSpam(threads);
-  }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-function parseEmailMessages(start) {
-
-  start = start || 0;
-
-  var threads = GmailApp.getInboxThreads(start, 100);
-  var sheet = SpreadsheetApp.getActiveSheet();
-
-  for (var i = 0; i < threads.length; i++) {
-
-  // Get the first email message of a threads
-    var tmp,
-      message = threads[i].getMessages()[0],
-      subject = message.getSubject(),
-      content = message.getPlainBody();
-
-    // Get the plain text body of the email message
-    // You may also use getRawContent() for parsing HTML
-
-    // Implement Parsing rules using regular expressions
-    if (content) {
-
-      tmp = content.match(/Name:\s*([A-Za-z0-9\s]+)(\r?\n)/);
-      var username = (tmp && tmp[1]) ? tmp[1].trim() : 'No username';
-
-      tmp = content.match(/Email:\s*([A-Za-z0-9@.]+)/);
-      var email = (tmp && tmp[1]) ? tmp[1].trim() : 'No email';
-
-      tmp = content.match(/Comments:\s*([\s\S]+)/);
-      var comment = (tmp && tmp[1]) ? tmp[1] : 'No comment';
-
-      sheet.appendRow([username, email, subject, comment]);
-
-    } // End if
-
-  } // End for loop
-}
-
-
-
-
-
-function storeValues (){
-
-    var ss = SpreadsheetApp.getActiveSpreadsheet();
-    var sheet = ss.getSheetByName ("Sheet 1");
-
-    var label = GmailApp.getUserLabelBy("MyLabel");
-    var threads = label.getThreads();
-
-    for (var i=0; i<threads.length; i++)
-    {
-      var details = threads[i].getDetails();
-
-      for (var j=0; j<details.length; j++)
-      {
-        var nam = details[j].getPlainName();
-        var dat = details[j].getPlainDate();
-
-        ss.appendRow([nam, dat])
-      }
-      threads[i].removeLabel(label);
-    }
-}
